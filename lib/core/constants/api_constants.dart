f5e2512a6b5ef9b637e621dfe6a33fec2ea71bcb
GAJAHGAJAH <1123150165@global.ac.id>
2026-04-24 03:04:42 +0700

    memperbaiki api constan

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // GANTI dengan IP laptop Anda. Jika pakai Android Emulator: http://10.0.2.2:8080/v1
-  static const String baseUrl = 'http://192.168.0.28:8080/v1';
+  static const String baseUrl = 'http://10.87.208.113:8080/v1';
   static const String verifyToken = '/auth/verify-token';
   static const String products = '/products';
   static const int connectTimeout = 15000;
